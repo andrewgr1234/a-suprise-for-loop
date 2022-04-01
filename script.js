ec586bd48056f6d1f9ed337fee47a0ec87d6a7de
andrewgr1234 <85031521+andrewgr1234@users.noreply.github.com>
2022-04-01 17:31:08 +0300

    changed the script for the slide show

--- script.js
+++ script.js
@@ -24,5 +24,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000);
+  setTimeout(showSlides, 2000);
 }
